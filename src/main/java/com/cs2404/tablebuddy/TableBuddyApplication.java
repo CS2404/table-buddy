b0f097b258dc837b24fea0f7e515a403f6168da5
seo jong guk <jongkuk.dev@gamil.com>
2024-09-28 22:18:58 +0900

    init

---
+++ src/main/java/com/cs2404/tablebuddy/TableBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.cs2404.tablebuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableBuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TableBuddyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cs2404/tablebuddy/TableBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs2404.tablebuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableBuddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
