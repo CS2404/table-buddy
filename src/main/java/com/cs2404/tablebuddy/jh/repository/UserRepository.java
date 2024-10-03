c7bd909ae136771e632e87ac9b1873d0f143a464
creatingeveryday <jongkuk.dev@gmail.com>
2024-10-03 23:52:15 +0900

    delete

--- src/main/java/com/cs2404/tablebuddy/jh/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cs2404.tablebuddy.jh.repository;
-
-import com.cs2404.tablebuddy.jh.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
-}
