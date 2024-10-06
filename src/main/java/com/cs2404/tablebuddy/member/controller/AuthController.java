06de118bab268dc632a7717d112ef47c92632149
creatingeveryday <jongkuk.dev@gmail.com>
2024-10-06 22:17:44 +0900

    fix: 불필요한 클래스 제거

--- src/main/java/com/cs2404/tablebuddy/member/controller/AuthController.java
+++ src/main/java/com/cs2404/tablebuddy/member/controller/AuthController.java
@@ -1,10 +1,11 @@
 package com.cs2404.tablebuddy.member.controller;
 
-import com.cs2404.tablebuddy.common.response.ApiResponse;
 import com.cs2404.tablebuddy.member.dto.MemberSignUpDto;
 import com.cs2404.tablebuddy.member.service.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +19,13 @@ public class AuthController {
     private final MemberService memberService;
 
     @PostMapping("/signup")
-    public ApiResponse<MemberSignUpDto.Response> signUp(
+    public ResponseEntity<MemberSignUpDto.Response> signUp(
             @Valid @RequestBody MemberSignUpDto.Request memberSignUpDto) {
 
         Long savedMemberId = memberService.signUp(memberSignUpDto);
 
-        return ApiResponse.success(new MemberSignUpDto.Response(savedMemberId));
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new MemberSignUpDto.Response(savedMemberId));
     }
 }
