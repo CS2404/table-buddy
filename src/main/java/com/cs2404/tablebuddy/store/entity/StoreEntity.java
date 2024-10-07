f3bd8d8fb643aa3584e8ff4265a780f3cdf94b90
jhmoon <sugaryfood@naver.com>
2024-10-07 23:19:35 +0900

    fix: 가게 entity 필드 상세정보 추가(#11)

--- src/main/java/com/cs2404/tablebuddy/store/entity/StoreEntity.java
+++ src/main/java/com/cs2404/tablebuddy/store/entity/StoreEntity.java
@@ -12,13 +12,20 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "store")
 public class StoreEntity extends BaseTimeEntity {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "store_id")
     private Long id;
 
@@ -26,17 +33,26 @@ public class StoreEntity extends BaseTimeEntity {
     @JoinColumn(name = "member_id")
     private MemberEntity member;
 
-    @Column(name = "name")
+    @Column(name = "name", length = 50, nullable = false)
     private String name;
 
     @Enumerated(value = EnumType.STRING)
-    @Column(name = "category")
+    @Column(name = "category", length = 20, nullable = false)
     private Category category;
 
-    @Column(name = "max_waiting_capacity")
+    @Column(name = "max_waiting_capacity", nullable = false)
     private Long maxWaitingCapacity;         // 최대 대기인원
 
-    @Column(name = "is_deleted")
+    @Column(name = "is_deleted", length = 1, nullable = false)
     private DeleteStatus isDeleted;
 
+    @Builder
+    public StoreEntity(Long id, MemberEntity member, String name, Category category, Long maxWaitingCapacity, DeleteStatus isDeleted) {
+        this.id = id;
+        this.member = member;
+        this.name = name;
+        this.category = category;
+        this.maxWaitingCapacity = maxWaitingCapacity;
+        this.isDeleted = isDeleted;
+    }
 }
