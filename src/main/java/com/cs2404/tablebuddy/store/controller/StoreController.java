7a20ac8310625e19392fa3f446931bc4ff76e4d8
jhmoon <sugaryfood@naver.com>
2025-03-05 22:16:17 +0900

    feat: 가게 다건 조회 api 구현

--- src/main/java/com/cs2404/tablebuddy/store/controller/StoreController.java
+++ src/main/java/com/cs2404/tablebuddy/store/controller/StoreController.java
@@ -10,6 +10,7 @@ import com.cs2404.tablebuddy.store.dto.UpdateStoreDto;
 import com.cs2404.tablebuddy.store.service.StoreService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -69,6 +71,16 @@ public class StoreController {
             @PathVariable Long storeId
     ) {
         return ResponseEntity.ok(storeService.getStore(storeId));
+    }
+
+    // 가게 다건 조회 API
+    @GetMapping()
+    public ResponseEntity<Page<StoreDto>> getStores(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        Page<StoreDto> stores = storeService.getStores(page, size);
+        return ResponseEntity.ok(stores);
     }
 
 }
