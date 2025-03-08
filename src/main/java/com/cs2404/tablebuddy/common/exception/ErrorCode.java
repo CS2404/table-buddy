0a97ebf2bb27aec6b4fd432e46376e629a1c3ba4
creatingeveryday <jongkuk.dev@gmail.com>
2025-03-08 22:31:31 +0900

    Fix: 중복 제거

--- src/main/java/com/cs2404/tablebuddy/common/exception/ErrorCode.java
+++ src/main/java/com/cs2404/tablebuddy/common/exception/ErrorCode.java
@@ -22,7 +22,6 @@ public enum ErrorCode {
     INVALID_BUSINESS_DAY(HttpStatus.BAD_REQUEST, "TBJ1001", "영업일을 잘 못 입력하셨습니다."),
     ALREADY_EXIST_STORE(HttpStatus.CONFLICT, "TBJ1002", "이미 등록한 가게가 존재합니다."),
     STORE_OWNER_MISMATCH(HttpStatus.BAD_REQUEST, "TBJ1003", "가게 소유자가 아닙니다."),
-    STORE_NOT_FOUND(HttpStatus.BAD_REQUEST, "TBJ1004", "가게를 찾을 수 없습니다." )
     ;
 
     private final String message;
